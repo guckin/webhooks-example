163a0dc3a1e47000a75ed69b82e67cc4a7c0c767
Stephen McGuckin <stephendmcguckin@gmail.com>
2023-09-18 15:55:03 -0700

    updates old rollup deps

--- rollup.config.js
+++ rollup.config.js
@@ -1,6 +1,6 @@
 import resolve from '@rollup/plugin-node-resolve';
 import commonjs from '@rollup/plugin-commonjs';
-import { terser } from 'rollup-plugin-terser';
+import { terser } from 'rollup-plugin-minification';
 import typescript from '@rollup/plugin-typescript';
 import json from '@rollup/plugin-json';
 
