7f53c437cbf4473465fc364c43ba172c76fb1d5e
Stephen McGuckin <stephendmcguckin@gmail.com>
2023-09-18 16:10:07 -0700

    updates officially supported minification package

--- rollup.config.js
+++ rollup.config.js
@@ -1,6 +1,6 @@
 import resolve from '@rollup/plugin-node-resolve';
 import commonjs from '@rollup/plugin-commonjs';
-import { terser } from 'rollup-plugin-minification';
+import { terser } from '@rollup/plugin-terser';
 import typescript from '@rollup/plugin-typescript';
 import json from '@rollup/plugin-json';
 
